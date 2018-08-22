0a475b800ff6bea6aec1fd33967082c03fe61a16
Noah Wager <noahwager@gmail.com>
2018-08-22 09:56:47 -0700

    Add files via upload

--- specs/AppSpec.js
+++ specs/AppSpec.js
@@ -1,4 +1,4 @@
-import { focusText, _keyboardDidShow } from './helpers';
+import { focusText, blueText, _keyboardDidShow, _keyboardDidHide, blurText } from './helpers';
 import { Keyboard } from 'react-native';
 
 export default function(spec) {
@@ -11,8 +11,12 @@ export default function(spec) {
         await spec.pause(1000);
         const text = await spec.findComponent('EditSceneName');
         text.keyboardListener = Keyboard.addListener('keyboardDidShow', _keyboardDidShow);
+        text.keyboardListener = Keyboard.addListener('keyboardDidHide', _keyboardDidHide);
         await focusText(text);
-        await spec.pause(10000);
+        await spec.pause(2000);
+        await blurText(text);
+        await spec.pause(10000)
+
       });
     });
   }
--- specs/helpers.js
+++ specs/helpers.js
@@ -3,7 +3,16 @@ export async function focusText(identifier) {
   identifier.focus();
 }
 
+export async function blurText(identifier) {
+  identifier.blur();
+}
+
 export async function _keyboardDidShow() {
-  alert("Keyboard did show");
+  //alert("Keyboard did show");
   throw new Error('Keyboard did show');
 }
+
+export async function _keyboardDidHide() {
+  alert("keyboard did hide");
+}
+
