3b5ca4ca8aec8fe235454330f8f5e690030fadd7
Noah Wager <noahwager@gmail.com>
2018-08-22 14:42:10 -0700

    Add files via upload

--- specs/AppSpec.js
+++ specs/AppSpec.js
@@ -4,16 +4,16 @@ import { Keyboard } from 'react-native';
 export default function(spec) {
     spec.describe('Renaming scene', function() {
       spec.it('works', async function() {
-        //await spec.pause(10000);
+        //await spec.pause(1000000);
         await spec.press('ScenePropPill');
         await spec.pause(1000);
-        await spec.fillIn('EditSceneName', "Hello");
         await spec.pause(1000);
         const text = await spec.findComponent('EditSceneName');
         text.keyboardListener = Keyboard.addListener('keyboardDidShow', _keyboardDidShow);
         text.keyboardListener = Keyboard.addListener('keyboardDidHide', _keyboardDidHide);
         await focusText(text);
-        await spec.pause(2000);
+        await spec.fillIn('EditSceneName', "New scene");
+        await spec.pause(1000);
         await blurText(text);
         await spec.pause(10000)
 
